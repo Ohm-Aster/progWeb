91b2bd2cf8f2648475b6f35b0821e879946b5e34
OhmAster <om_art1721@hotmail.com>
2022-05-06 09:23:30 -0500

    Add files via upload

--- crud/pie.php
+++ crud/pie.php
@@ -1,7 +1,6 @@
 </main>
-<footer class="px-2 py-2 fixed-bottom bg-dark">
-        <span class="text-muted">CRUD de PHP</a>
-        </span>
+    <span>CRUD de PHP</a>
+    </span>
 </footer>
 </body>
 </html>
